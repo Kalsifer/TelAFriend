d3e9fafcdc5251e7c442851ec3e66304c2592914
Alana Morin <daddyslittlebyte@gmail.com>
2015-06-22 12:36:46 -0700

    Added slider, changed around images

---
+++ js/slicksettings.js
@@ -0,0 +1,15 @@
+/**
+ * Created by alani on 2015-06-21.
+ */
+
+
+$(document).ready(function(){
+
+    $('.fade2').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
